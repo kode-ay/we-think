c86259038a316e387a719454e34a0426938ca048
Ibhasebhor Kingsley <ibhasebhorkingsley@gmail.com>
2023-09-12 07:49:16 +0100

    Development

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,13 @@
 import './globals.css'
 import { Montserrat } from 'next/font/google'
 import { Inter } from 'next/font/google'
+import { Roboto_Condensed } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 const inter = Inter({ subsets: ['latin'] })
+// const roboto_condensed = Roboto_Condensed({ subsets: ['latin'], weight: })
+// const roboto = Roboto({ subsets: ['latin'], weight: })
 
 export const metadata = {
   title: 'We Think',
--- app/page.js
+++ app/page.js
@@ -3,18 +3,31 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="">
-      <h1 className="flex items-center gap-2 p-5 justify-center text-2xl">
-        <Image src="/we think.png" width={200} height={200} />
+    <main className="px-40 py-20 flex justify-between items-center h-screen bg-grad text-white">
+      <div className="text flex flex-col justify-between items-start h-full max-w-lg">
+        <Image src="/we think/we think.png" width={192} height={62} />
+        <h1 className="font-bold text-7xl">cool projects for you!</h1>
+        <ul className="links flex flex-col gap-8">
+          <li>
+            <Link href="/confiplay" className='opacity-70'>&gt; Confi-PLAY</Link>
+          </li>
+          <li>
+            <Link href="/enola" className='opacity-70'>&gt; Enola</Link>
+          </li>
+        </ul>
+        <h3>welcome</h3>
+      </div>
+      {/* <h1 className="flex items-center gap-2 p-5 justify-center text-2xl">
+        <Image src="/we think/we think.png" width={200} height={200} />
       </h1>
       <div className="links flex justify-center gap-10 items-center p-10">
-        <Link href="/enola" className='px-8 py-5 rounded-lg bg-slate-100'>
+        <Link href="/enola" className="px-8 py-5 rounded-lg bg-slate-100">
           <Image src="/enola/enola.png" width={150} height={150} />
         </Link>
-        <Link href="/confiplay" className='p-8 rounded-lg bg-sky-700'>
+        <Link href="/confiplay" className="p-8 rounded-lg bg-sky-700">
           <Image src="/confiplay/confiplay.png" width={150} height={150} />
         </Link>
-      </div>
+      </div> */}
     </main>
   );
 }
